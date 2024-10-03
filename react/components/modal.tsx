246ea2a0427c73ea15b801d65cb86f42171aaf19
thamaraikannan <thamaraik69@gmail.com>
2024-10-03 11:20:56 +0530

    Modal component

--- react/components/modal.tsx
+++ react/components/modal.tsx
@@ -5,29 +5,44 @@ interface ModalInterface {
   header: string;
   body: string;
   footer: string;
+  onClose: () => void;
 }
 
 export default function Model() {
   const [ShowModalPopUp, setShowModalPopUp] = useState(false);
+
+  const handleClose = () => {
+    setShowModalPopUp(false); 
+  };
+
   return (
     <>
-      <button onClick={() => setShowModalPopUp(!ShowModalPopUp)} className="modal__btn">
+      <button
+        onClick={() => setShowModalPopUp(true)}
+        className="modal__btn"
+      >
         Open Modal
       </button>
       {ShowModalPopUp && (
-        <ShowModal id={1} header="Header" body="Body" footer="Footer" />
+        <ShowModal
+          id={1}
+          header="Header"
+          body="Body"
+          footer="Footer"
+          onClose={handleClose}
+        />
       )}
     </>
   );
 }
 
-const ShowModal = ({ id, header, body, footer }: ModalInterface) => {
+const ShowModal = ({ id, header, body, footer, onClose }: ModalInterface) => {
   return (
     <>
       <div id={id || "Modal"} className="modal__wrap">
         <div className="modal__container">
           <div className="modal__header">
-            <span className="modal__header--cls"></span>
+            <span className="modal__header--cls" onClick={onClose}></span>
             <h2>{header}</h2>
           </div>
           <div className="modal__body">{body}</div>
